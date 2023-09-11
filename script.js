81a55f639586e875dc2a1163b3e64bff3ec4f37c
Frank <frank2010dk@gmail.com>
2023-09-11 10:08:13 -0500

    Portafolio 1.0.2

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-$(function(){
-    $('.menu-icon').click(function() {
-        $('header nav').slideToggle();
-    });
+// Función para mostrar el menú en pantallas más anchas
+function showMenuOnLargeScreens() {
+    if ($(window).width() >= 480) {
+        $('header nav').show();
+    } else {
+        $('header nav').hide();
+    }
+}
+
+// Ejecutar la función al cargar la página
+$(document).ready(function() {
+    showMenuOnLargeScreens();
+});
+
+// Ejecutar la función en eventos de redimensionamiento de la ventana
+$(window).resize(function() {
+    showMenuOnLargeScreens();
+});
+
+// Manejar el clic en el ícono del menú
+$('.menu-icon').click(function() {
+    $('header nav').slideToggle();
 });
